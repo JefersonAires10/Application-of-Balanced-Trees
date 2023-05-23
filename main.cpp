0454aa2c6f8fd90511eb411f940dc7db949f6f0e
Jeferson Aires <111990809+JefersonAires10@users.noreply.github.com>
2023-05-22 22:54:46 -0300

    implementação inicializada

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <regex>
 #include "pessoa.h"
 #include "avl.h"
 #include "node.h"
 #include "data.h"
+#include "menus.h"
 
 using namespace std;
 
@@ -38,7 +40,7 @@ void readFile(vector<Pessoa> &pessoas, string filename) {
         ss2 >> dia;
         ss2.ignore(1);
         ss2 >> ano;
-        Date dataDeNascimento(dia, mes, ano);
+        Date dataDeNascimento(mes, dia, ano);
 
         Pessoa pessoa(cpf, nome + " " + sobrenome, dataDeNascimento);
         pessoas.push_back(pessoa);
@@ -62,28 +64,102 @@ int main() {
     vector<Pessoa> pessoas;
     readFile(pessoas, "data.csv");
 
-    //cout << "Tamanho do arquivo: " << pessoas.size() << endl;
-
     avl_tree<string> Cpf;
     avl_tree<string> Nome;
     avl_tree<Date> data_nascimento;
 
     preencherArvores(Cpf, Nome, data_nascimento, pessoas);
 
-    /*cout << "Arvore de CPFs: " << endl;
-    Cpf.bshow();
-    cout << endl;*/
-    
-    //Cpf.searchCpf(Cpf, "607.941.973-45");
-    
-    Nome.searchName(Nome, "J");
+    while (true) {
+        int op;
+        menuInicial();
+        cout << "Digite a opção desejada: ";
+        cin >> op;
 
-    /*cout << "Arvore de Nomes: " << endl;
-    Nome.bshow();
-    cout << endl;*/
-    
-    /*cout << "Arvore de Datas de Nascimento: " << endl;
-    data_nascimento.bshow();
-    cout << endl;*/
+        if (op == 1) {
+            system("cls || clear");
+            while (true) {
+                menuPrincipal();
+                int opcao;
+                cout << "Digite a opção desejada: ";
+                cin >> opcao;
+                if (opcao == 1) {
+                    cout << "Digite o CPF da pessoa para exibir os seus dados: ";
+                    string cpf;
+                    cin >> cpf;
 
+                    regex cpfRegex(R"(\d{3}\.\d{3}\.\d{3}-\d{2})");
+                    if (regex_match(cpf, cpfRegex)) {
+                        system("cls || clear");
+                        cout << "Busca realizada, segue o retorno da operação: " << endl;
+                        cout << endl;
+                        Cpf.searchCpf(Cpf, cpf);
+                        cout << endl;
+                    }
+                    else {
+                        cout << endl;
+                        system("cls || clear");
+                        cout << "O Cpf informado é inválido, digite uma string no formato: 000.000.000-00" << endl;
+                        cout << endl;                        
+                    }
+                }
+                else if (opcao == 2) {
+                    cout << "Se você procura alguém específico, digite o nome completo desta pessoa: " << endl;
+                    system("pause");
+                    cout << "Caso contrário, digite apenas uma string qualquer: ";
+                    string nome;
+                    cin.ignore();
+                    getline(cin, nome);
+                    system("cls || clear");
+                    Nome.searchName(Nome, nome);
+                }
+                /*else if (opcao == 3) {
+                    cout << "Digite a data de nascimento da pessoa que você procura: ";
+                    string data;
+                    cin >> data;
+                    regex dataRegex(R"(\d{2}\/\d{2}\/\d{4})");
+                    if (regex_match(data, dataRegex)) {
+                        stringstream ss(data);
+                        int dia, mes, ano;
+                        ss >> dia;
+                        ss.ignore(1);
+                        ss >> mes;
+                        ss.ignore(1);
+                        ss >> ano;
+                        Date dataDeNascimento(mes, dia, ano);
+                        system("cls || clear");
+                        data_nascimento.searchDate(data_nascimento, dataDeNascimento);
+                    }
+                    else {
+                        cout << endl;
+                        system("cls || clear");
+                        cout << "A data informada é inválida, digite uma string no formato: dd/mm/aaaa" << endl;
+                        cout << endl;
+                    }
+                }*/
+                else if (opcao == 4) {
+                    system("cls || clear");
+                    break;
+                }
+                else {
+                    cout << endl;
+                    system("cls || clear");
+                    cout << "Opção inválida, digite uma opção válida." << endl;
+                    cout << endl;
+                }
+            }
+        }
+
+        else if (op == 2) {
+            system("cls || clear");
+            menuOpAdicionais();
+
+        }
+
+        else if (op == 3) {
+            system("cls || clear");
+            menuEncerramento();
+            break;
+        }        
+    }
 }
