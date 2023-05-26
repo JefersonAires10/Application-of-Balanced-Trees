f23d209d1bc827316297b65aa987d9fedfba33b7
Jeferson Aires <111990809+JefersonAires10@users.noreply.github.com>
2023-05-26 20:57:53 -0300

    documentação do código

--- avl.h
+++ avl.h
@@ -12,62 +12,100 @@ using namespace std;
 template<typename T>
 class avl_tree {
 public:
+
+    /**
+     * @brief Construtor default da classe avl_tree
+     * 
+    */
     avl_tree() = default;
     
+    /**
+     * @brief Construtor da classe avl_tree
+     * 
+     * @param t 
+     */
     avl_tree(const avl_tree& t); 
-    
-    avl_tree& operator=(const avl_tree& t) {
-        if(this != &t) {
-            clear();
-            root = clone_rec(t.root);
-        }
-        return *this;
-    } 
-    
+
+    /**
+     * @brief Função para printar a árvore organizada
+     * @param root
+     * */    
     void bshow() const { bshow(root, ""); }
 
+    /**
+     * @brief Destrutor da classe avl_tree
+    */
     ~avl_tree() { clear(); }
 
+    /**
+     * @brief Função para adicionar um novo nó na árvore
+     * @param Key tipo T, Ponteiro para Pessoa
+    */
     void add(T key, Pessoa *pessoa) { root = add(root, key, pessoa); }    
 
-    void clear() { root = clear(root); }                           
+    /**
+     * @brief Função para limpar a árvore
+     * @param root
+    */
+    void clear() { root = clear(root); } 
 
+    /**
+     * @brief Função de busca na árvore
+     * @param root, Key tipo T
+    */
     Node<T>* search(T key) { return search(root, key); }                          
 
+    /**
+     * @brief Função de busca na árvore de nomes, através do nome (completo)
+     * @param referencia para a árvore de nomes, Key tipo T 
+    */
     void exibirCidade(const avl_tree<T>& arvoreNome, T key) {
-        Node<T> *nodeNome = search(key);
-        if (nodeNome != nullptr) {
-            Pessoa *pessoa = nodeNome->pessoa;
-            cout << "A(o) " << nodeNome->key << " é de " << pessoa->getCidade() << endl;
+        Node<T> *nodeNome = search(key); // Busca o nó na árvore de nomes através da search
+        if (nodeNome != nullptr) { // Se o nó existir
+            Pessoa *pessoa = nodeNome->pessoa; // Eu tenho um ponteiro pra pessoa, atribuo o ponteiro do nó para o ponteiro da pessoa
+            cout << "A(o) " << nodeNome->key << " é de " << pessoa->getCidade() << endl; // Pego o getCidade da pessoa
         }
         else {
-            cout << "O nome informado é inexistente!" << endl;
+            cout << "O nome informado é inexistente!" << endl; // Se o nó não existir
         }
     }
     
-    // Consultar uma única pessoa pelo seu CPF e exibir seus dados na tela
+    /**
+     * @brief Função de consulta através do CPF na árvore de CPFs
+     * @param referencia para a árvore de CPFs, Key tipo T
+    */
     void searchCpf(const avl_tree<T>& arvoreCpf, T key) {
-        Node<T> *nodeCpf = search(key);
-        if(nodeCpf != nullptr) {
-            Pessoa *pessoa = nodeCpf->pessoa;
-            cout << "CPF: " << nodeCpf->key << endl;
+        Node<T> *nodeCpf = search(key); // Busca o nó na árvore de CPFs através da search, pois o cpf é único para cada pessoa
+        if(nodeCpf != nullptr) { // Se o nó existir
+            Pessoa *pessoa = nodeCpf->pessoa; // Eu tenho um ponteiro pra pessoa, atribuo o ponteiro do nó para o ponteiro da pessoa
+            /*Imprimo as informações do nó encontrado, através das funções get, da classe Pessoa*/
+            cout << "CPF: " << nodeCpf->key << endl; 
             cout << "Nome: " << pessoa->getNome() << endl;
             cout << "Data de nascimento: " << pessoa->getDataNascimento() << endl;
         }
         else {
-            cout << "O Cpf informado é inexistente!" << endl;
+            cout << "O Cpf informado é inexistente!" << endl; // Se o nó não existir
         }
     }
 
+    /**
+     * @brief Função pública para a consulta de pessoas por prefixo
+     * @param referencia para a árvore de nomes, referencia para T key 
+    */
     void searchName(const avl_tree<T>& arvoreNome, const T& key) {
         searchNameHelper(arvoreNome.root, key);
     }
 
+    /**
+     * @brief Função pública para a consulta de pessoas por um intervalo de datas
+     * @param referencia para a árvore de datas, referencia para T dataInicial, referencia para T dataFinal
+    */
     void searchDate(const avl_tree<T>& arvoreData, const T& dataInicial, const T& dataFinal) {
         searchDateHelper(arvoreData.root, dataInicial, dataFinal);
     }
     
 private:
+
     Node<T> *root {nullptr};            
 
     int height(Node<T> *node) { return (node == nullptr) ? 0 : node->height; }
@@ -76,22 +114,49 @@ private:
     // Consultar todas as pessoas cujo nome comece com uma string informada pelo
     // usuário e exibir na tela todos os dados dessas pessoas na forma de lista.
 
+    /**
+     * @brief Função privada para consulta de nome por prefixo
+     * @param ponteiro para o Node nome e uma referencia constante para T key
+    */
     void searchNameHelper(const Node<T>* nodeNome, const T& key) {
-        if (nodeNome == nullptr) {
-            return;
+        if (nodeNome == nullptr) { // Se o nó for nulo, retorna
+            return; // continue
         }
 
-        if (nodeNome->key.compare(0, key.length(), key) == 0) {
-            // O prefixo das strings são iguais
-            Pessoa *pessoa = nodeNome->pessoa;
-            std::cout << nodeNome->key << std::endl;
+        /*
+            Aqui é feita a comparação do prefixo da chave key com o prefixo da chave do nó,
+            através da função compare, uma função da bibilioteca string, que retorna 0 se as strings forem iguais.
+            A função compara o valor de key com o valor do nó, até o tamanho da string key.
+            Onde key é o prefixo que o usuário digitou e nodeNome->key é o prefixo da chave do nó atual
+            O primeiro parametro é o indice inicial da string, ou seja, de onde vai começar a comparação
+            O segundo parametro pega o tamanho da string que vai ser comparada
+            E o terceiro parametro é a string que vai ser comparada.
+        */
+        if (nodeNome->key.compare(0, key.length(), key) == 0) { // Se todo o valor da comparação for igual a 0
+            // O quer diz que o prefixo da chave do nó é igual ao prefixo da chave key
+            Pessoa *pessoa = nodeNome->pessoa; // atribuo o ponteiro do nó para o ponteiro da pessoa
+            std::cout << nodeNome->key << std::endl; // imprimo a chave do nó
+            /* E imprimo os dados da pessoa através das funções get, da classe Pessoa.*/
             std::cout << "Nome: " << pessoa->getNome() << std::endl;
             std::cout << "CPF: " << pessoa->getCpf() << std::endl;
             std::cout << "Data de nascimento: " << pessoa->getDataNascimento() << std::endl;
             cout << endl;
 
-            if (!nodeNome->sameKey.empty()) {
-                for (const auto& pessoa : nodeNome->sameKey) {
+            /*
+            Na minha classe Node, eu tenho um vector de ponteiros para pessoas, chamado sameKey.
+            Eu vou preenchendo esse vector com os ponteiros para as pessoas que tem o mesmo prefixo.
+            Então, se o vector não estiver vazio, eu percorro ele e imprimo os dados das pessoas.   
+            */
+            if (!nodeNome->sameKey.empty()) { // Se o vector não estiver vazio
+                /*
+                Fiz usando um for range, que itera sobre os elementos do vector
+                Cada elemento é acessado por referencia, uso o auto para que o tipo seja deduzido pelo compilador
+                */
+                for (const auto& pessoa : nodeNome->sameKey) { // Para cada pessoa no vector
+                    /* 
+                    imprimo também as informações dessas pessoas, pois a busca é por prefixo
+                    e podemos ter mais de uma pessoa com o mesmo prefixo
+                    */
                     std::cout << "CPF: " << pessoa->getCpf() << std::endl;
                     std::cout << "Nome: " << pessoa->getNome() << std::endl;
                     std::cout << "Data de nascimento: " << pessoa->getDataNascimento() << std::endl;
@@ -99,7 +164,10 @@ private:
                 }
             }
         }
-        if (key[0] <= nodeNome->key[0] && nodeNome->left != nullptr) {
+        /*
+        
+        */
+        if (key[0] <= nodeNome->key[0] && nodeNome->left != nullptr) { 
             searchNameHelper(nodeNome->left, key);
         }
         
