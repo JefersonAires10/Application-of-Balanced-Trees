7ad68833e242bfe229cb80fcc42e92aa607f7013
Jeferson Aires <111990809+JefersonAires10@users.noreply.github.com>
2023-05-27 22:16:11 -0300

    classe documentada

--- pessoa.h
+++ pessoa.h
@@ -3,6 +3,9 @@
 #include <string>
 #include "data.h"
 
+/**
+ * @brief Classe Pessoa que contém os atributos de uma pessoa
+*/
 class Pessoa {
     private:
         std::string cpf;
@@ -13,16 +16,47 @@ class Pessoa {
     public:
         Pessoa() = default; // Construtor padrão
         // Construtor
+        /**
+         * @brief Construtor da classe Pessoa
+        */
         Pessoa(std::string cpf, std::string nome, Date dataDeNascimento, std::string cidade) : cpf(cpf), nome(nome), dataDeNascimento(dataDeNascimento), cidade(cidade) {}
 
-        // Getters e Setters
+        /**
+         * @brief getCpf da classe Pessoa
+         * @return cpf
+        */
         std::string getCpf() { return this->cpf; }
+        /**
+         * @brief setCpf da classe Pessoa   
+         * @param novoCpf
+        */
         void setCpf(int novoCpf) { this->cpf = novoCpf; }
+
+        /**
+         * @brief getNome da classe Pessoa
+         * @return nome
+        */
         std::string getNome() { return this->nome; }
+        /**
+         * @brief setNome da classe Pessoa
+         * @param novoNome
+        */
         void setNome(std::string novoNome) { this->nome = novoNome; }
+        /**
+         * @brief getDataNascimento da classe Pessoa
+         * @return dataDeNascimento
+        */
         Date& getDataNascimento() { return this->dataDeNascimento; }
         void setDataNascimento(Date novaData) { this->dataDeNascimento = novaData; }
+        /**
+         * @brief getCidade da classe Pessoa
+         * @return cidade
+        */
         std::string getCidade() { return this->cidade; }
+        /**
+         * @brief setCidade da classe Pessoa
+         * @param novaCidade
+        */
         void setCidade(std::string novaCidade) { this->cidade = novaCidade; }
     
 };
