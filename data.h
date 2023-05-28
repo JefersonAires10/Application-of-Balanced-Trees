8b81268275561683108b0bddf20bc7b8d87de565
Jeferson Aires <111990809+JefersonAires10@users.noreply.github.com>
2023-05-28 12:14:55 -0300

    classe documentada

--- data.h
+++ data.h
@@ -17,19 +17,41 @@ struct Date {
         this->mes = mes;
         this->ano = ano;
     }
-
+    /**
+     * @brief getDia da classe Date
+     * @return dia
+    */
     int getDia() { return this->dia; }
-
+    /**
+     * @brief setDia da classe Date
+     * @param dia
+    */
     void setDia(int dia) { this->dia = dia; }
-
+    /**
+     * @brief getMes da classe Date
+     * @return mes
+    */
     int getMes() { return this->mes; }
-
+    /**
+     * @brief setMes da classe Date
+     * @param mes
+    */
     void setMes(int mes) { this->mes = mes; }
-
+    /**
+     * @brief getAno da classe Date
+     * @return ano
+    */
     int getAno() { return this->ano; }
-
+    /**
+     * @brief setAno da classe Date
+     * @param ano
+    */
     void setAno(int ano) { this->ano = ano; }
-
+    /**
+     * @brief operator< da classe Date
+     * @param data 
+     * @return true se a data for menor que a data passada por parâmetro
+    */
     bool operator<(Date data) {
         if (this->getAno() < data.getAno()) { 
             return true; 
@@ -57,7 +79,11 @@ struct Date {
             return false; 
         }
     }
-
+    /**
+     * @brief operator> da classe Date
+     * @param data
+     * @return true se a data for maior que a data passada por parâmetro
+    */
     bool operator>(Date data) {
         if (this->getAno() > data.getAno()) {
             return true;
@@ -86,7 +112,11 @@ struct Date {
         }
        
     }
-
+    /**
+     * @brief operator== da classe Date
+     * @param data
+     * @return true se a data for igual a data passada por parâmetro
+    */
     bool operator==(Date data) { 
         if (this->getAno() == data.getAno() && this->getMes() == data.getMes() && this->getDia() == data.getDia()) {
             return true;
@@ -95,7 +125,11 @@ struct Date {
             return false;
         }
     }
-
+    /**
+     * @brief operator!= da classe Date
+     * @param data
+     * @return true se a data for diferente da data passada por parâmetro
+    */
     bool operator!=(Date data) {
         if (this->getAno() != data.getAno() || this->getMes() != data.getMes() || this->getDia() != data.getDia()) {
             return true;
@@ -104,15 +138,27 @@ struct Date {
             return false;
         }
     }
-
+    /**
+     * @brief operator>= da classe Date
+     * @param data
+     * @return true se a data for maior ou igual a data passada por parâmetro
+    */
     bool operator>=(Date data) {
         return (*this > data || *this == data);
     }
-
+    /**
+     * @brief operator<= da classe Date
+     * @param data
+     * @return true se a data for menor ou igual a data passada por parâmetro
+    */
     bool operator<=(Date data) {
         return (*this < data || *this == data);
-    }
-
+    }  
+    /**
+     * @brief operator<< da classe Date
+     * @param os, data
+     * @return os
+    */
     friend std::ostream& operator<<(std::ostream& os, Date& data)
     {
         os << data.mes << "/" << data.dia << "/" << data.ano;
